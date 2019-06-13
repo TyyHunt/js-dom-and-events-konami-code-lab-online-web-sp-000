92a6964a77e58fab3a865c7444d2a5f5c4711c9a
Tyler Hunt <tyhunt04@gmail.com>
2019-06-13 22:47:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,28 +4,28 @@ function init() {
   // a key map of allowed keys
 
 // a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+var codePosition = 0;
 
 // add keydown event listener
 document.addEventListener('keydown', function(e) {
   // get the value of the key code from the key map
   var key = allowedKeys[e.keyCode];
   // get the value of the required key from the konami code
-  var requiredKey = konamiCode[konamiCodePosition];
+  var requiredKey = code[codePosition];
 
   // compare the key with the required key
   if (key == requiredKey) {
 
     // move to the next key in the konami code sequence
-    konamiCodePosition++;
+    codePosition++;
 
     // if the last key is reached, activate cheats
-    if (konamiCodePosition == konamiCode.length) {
+    if (codePosition == code.length) {
       alert('Congratulations!');
-      konamiCodePosition = 0;
+      codePosition = 0;
     }
   } else {
-    konamiCodePosition = 0;
+    codePosition = 0;
   }
 });
 
