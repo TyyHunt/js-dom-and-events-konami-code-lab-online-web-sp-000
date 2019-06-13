0da31b04d54b1bc0ed1713d67d2c2e350025c105
Tyler Hunt <tyhunt04@gmail.com>
2019-06-13 22:59:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let codePosition = 0;
+
 function init() {
-  // Write your JavaScript code inside the init() function
+
+
+document.body.addEventListener('keydown', function(e) {
+
+  const key = parseInt(e.detail || e.which);
+
+  if (key == code[codePosition]) {
+
+    codePosition++;
+
+    if (codePosition == code.length) {
+      alert("Congratulations!");
+      codePosition = 0;
+    }
+  } else {
+    codePosition = 0;
+  }
+});
 
 }
